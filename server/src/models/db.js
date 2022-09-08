7d63ba2c85046175d8cb8f23e027b67d89a33bdb
galiardi <galiardi.dev@gmail.com>
2022-09-08 07:17:55 -0400

    heroku postgres connection

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -2,11 +2,20 @@ const { Pool } = require("pg");
 const { db } = require("../../config");
 
 const pool = new Pool({
-  user: db.user,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  database: db.database,
+  // local postgres connection
+
+  // user: db.user,
+  // password: db.password,
+  // host: db.host,
+  // port: db.port,
+  // database: db.database,
+
+  // heroku postgres connection
+
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
